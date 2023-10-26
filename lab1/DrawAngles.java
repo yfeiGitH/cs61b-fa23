aba545f3afc6b3e57e9a573d3f75edfca3fc6d08
yafei <yfei_li@outlook.com>
2023-10-26 16:00:24 -0600

    add DrawAngles.java

---
+++ lab1/DrawAngles.java
@@ -0,0 +1,12 @@
+public class DrawAngles{
+    public static void main(String[] args){
+        for(int i =1, i <=5, i++){
+            int j = 0;
+            while(j < i){
+                System.out.print("*");
+                j += 1;
+            }
+            System.out.println();
+        }
+    }
+}
