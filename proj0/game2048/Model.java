5b92b1dd536dc9869e508e3da3658df88f73b93e
yafei <yfei_li@outlook.com>
2023-10-28 15:43:13 -0600

    Add maxTileExists

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -154,7 +154,14 @@ public class Model extends Observable {
      * given a Tile object t, we get its value with t.value().
      */
     public static boolean maxTileExists(Board b) {
-        // TODO: Fill in this function.
+        int size = b.size();
+        for (int i = 0; i < size; i++){
+            for (int j = 0; j < size; j ++){
+                if (b.tile(i,j)!= null && b.tile(i,j).value() == MAX_PIECE){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
