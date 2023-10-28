2596213db0e7f96771af1284a146ae203f50a1d8
yafei <yfei_li@outlook.com>
2023-10-28 15:35:44 -0600

    Add EmptySpaceTest

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -137,7 +137,14 @@ public class Model extends Observable {
      *  Empty spaces are stored as null.
      * */
     public static boolean emptySpaceExists(Board b) {
-        // TODO: Fill in this function.
+        int size = b.size();
+        for (int i= 0; i < size; i++){
+            for (int j =0; j< size; j ++){
+                if (b.tile(i,j)== null){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
